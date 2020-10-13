29e3e20d8d86e41f3f94bdb37dd343c217965f4c
Martin Jelenak <studiolang@gmail.com>
2020-10-14 00:40:58 +0200

    add body-parser, cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const Joi = require('joi');
 const app = express()
 const mongoose = require('mongoose');
 const uri = process.env.MONGOATLAS_URI || "mongodb+srv://Langos:TestovacieHeslo2@cluster0.t0cqe.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority"
@@ -26,7 +27,19 @@ app.get('/api/user/:id', (req, res) => {
 });
 
 
+function validateUser(user, required = true) {
+    const schema = {
+        name: Joi.string(),
+        email: Joi.string(),
+        password: Joi.string(),
+        phone: Joi.string(),
+    };
+
+    return Joi.validate(user, schema, { presence: (required) ? "required" : "optional" });
+}
+
 app.post('/api/user', (req, res) => {
+    // const { error } = validateUser(req.body);
     User.create(req.body)
         .then(result => { res.json(result) })
         .catch(err => { res.send("Nepodařilo se uložit film!") });
@@ -34,21 +47,10 @@ app.post('/api/user', (req, res) => {
 
 const userSchema = new mongoose.Schema({
     name: String,
-    password: String,
     email: String,
+    password: String,
     phone: String,
 });
-
-// function validateUser(movie, allRequired = true) {
-//     const schema = {
-//         name: Joi.string().min(3),
-//         email: Joi.string(),
-//         password: Joi.string(),
-//         phone: Joi.bool(),
-//     };
-
-//     return Joi.validate(movie, schema, { presence: (allRequired) ? "required" : "optional" });
-// }
 
 const User = mongoose.model('User', userSchema);
 
